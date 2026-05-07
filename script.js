39c7525b1f748a746adcd1b6832260567ae1734b
Paava123 <bkalininsci@gmail.com>
2026-05-07 21:16:54 +0200

    commited my personal project by mistake =(

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+
+
 function Submit(){
 
 document.getElementById("main").classList.add("hidden");
@@ -9,8 +11,8 @@ document.getElementById("state").classList.remove("hidden");
 
  function  Rate(number){
     let rate = 0; 
-
-const arr =  document.getElementsByClassName("orange");
+let arr;
+arr =  document.getElementsByClassName("orange");
 
 while(arr.length > 0){
     arr[0].classList.remove("orange");
